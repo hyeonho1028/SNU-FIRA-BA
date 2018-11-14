07a78030f8b6fe03a52b161b4354f760c41f87f6
hyeonho1028 <gusgh1510@naver.com>
2018-11-14 21:20:32 +0900

    commit

--- 1 semester/R programing/memory.R
+++ 1 semester/R programing/memory.R
@@ -9,13 +9,20 @@ if(!require(ABC)){install.packages("ABC"); library(ABC)}
 # 패키지 설치
 
 
+# ---
+#   title: "organized_02"
+# author: "Hyeonho Lee"
+# date: "2018년 10월 7일"
 # output: 
 #   pdf_document: 
-#   latex_engine: xelatex
+#   toc: true
+# latex_engine: xelatex
 # html_document: default
 # word_document:
 #   highlight: tango
 # mainfont: NanumGothic
 # header-includes :
 #   - \usepackage{kotex}
-# R markdown
+# - \usepackage{setspace}
+# - \usepackage{booktabs}
+# ---
